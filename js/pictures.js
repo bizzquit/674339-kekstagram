6bd404c211c9d620bbb17351bb4cb877db5b3cdc
Serg <36944165+bizzquit@users.noreply.github.com>
2018-11-21 23:44:06 +0500

    substituted the data to the image (small and large), it remains to make comments

--- js/pictures.js
+++ js/pictures.js
@@ -3,6 +3,7 @@
 var minPictureLike = 15; // минимальное кол-во лайков
 var maxPictureLike = 200; // максимальное кол-во лайков
 var sumPictures = 25; // Кол-во фотографий других пользователей
+var maxComment = 15; // Максимальное кол-во комментов
 
 /* 1)
      1.1) Создаю 2 массива с вариантами comments and descriptions;
@@ -33,20 +34,27 @@ var pictures = [];
 for (var i = 1; i <= sumPictures; i++) {
   /* Определяем случайное описание */
   var description = Math.floor(Math.random() * descriptions.length);
-  /* Определяем случайный коммент */
-  var comment = Math.floor(Math.random() * comments.length);
+  /* Определяем случайный коммент и случайное кол-во */
+  var commentArr = [];
+  var randomNumber = Math.floor(Math.random() * maxComment) + 1;
+  for (var n = 0; n < randomNumber; n++) {
+    var commentRandom = Math.floor(Math.random() * comments.length);
+    var commentObject = ({
+      comments: comments[commentRandom]
+    });
+    commentArr.push(commentObject);
+  }
   /* Определяем случайное кол-во лайков */
   var like = Math.floor(Math.random() * (maxPictureLike - minPictureLike) + minPictureLike);
   var object = ({
     url: 'photos/' + i + '.jpg',
     likes: like,
-    comments: comments[comment],
+    comments: commentArr,
     description: descriptions[description]
   });
   /* добавляем элемент в конец массива*/
   pictures.push(object);
 }
-
 
 /* 2) Создание Шаблона  */
 
@@ -62,18 +70,35 @@ var imageSrc = similarPictureTemplate.querySelector('.picture__img');
 var commentText = similarPictureTemplate.querySelector('.picture__comments');
 var sumLikes = similarPictureTemplate.querySelector('.picture__likes');
 
-
-for (i = 0; i < 25; i++) {
+var fragment = document.createDocumentFragment();
+for (i = 0; i < sumPictures; i++) {
   imageSrc.setAttribute('src', pictures[i].url); // Вставляем адрес картинки
-  commentText.textContent = pictures[i].comments[comment].length; // Вставляем кол-во комментов
+  commentText.textContent = pictures[i].comments.length; // Вставляем кол-во комментов
   sumLikes.textContent = pictures[i].likes; // Вставляем кол-во лайков
   /* Клонируем шаблон*/
   var pictureTemplate = similarPictureTemplate.cloneNode(true);
   /* добавляем в родителя*/
-  parentBlog.appendChild(pictureTemplate);
+  fragment.appendChild(pictureTemplate);
 }
+parentBlog.appendChild(fragment);
 
+/* Вставляем данные 1-го элемента массива фото в блок big-picture */
+var bigPicturesBlock = document.querySelector('.big-picture');
+var elementArr = 0; // 1-й элемент массива pictures[]
+bigPicturesBlock.classList.remove('hidden');
+bigPicturesBlock.querySelector('.big-picture__img img').setAttribute('src', pictures[0].url);
+bigPicturesBlock.querySelector('.likes-count').textContent = pictures[elementArr].likes;
+bigPicturesBlock.querySelector('.social__caption').textContent = pictures[elementArr].description;
+bigPicturesBlock.querySelector('.comments-count').textContent = pictures[elementArr].comments.length;
 
+/* Вставляем комментарии */
+/* var socialCommentsBlocks = bigPicturesBlock.querySelector('.social__comments');
+var socialCommentsBlock = bigPicturesBlock.querySelector('.social__comment').cloneNode(true);
+
+var avatar = Math.floor(Math.random() * 6) + 1;
+
+console.log(socialCommentsBlocks.children.length);
+console.log(avatar);*/
 /*
 console.log('Случайный комментарий: - " ' + comments[comment] + ' "');
 console.log('Случайное описание: - " ' + descriptions[description] + ' "');
